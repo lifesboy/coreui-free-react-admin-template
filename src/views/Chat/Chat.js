1030a358171df966e8d7f7b772a966543e114a9e
tuan.nv <tuan.nv@citigo.com.vn>
2020-04-06 21:50:25 +0700

    init contact table status

--- src/views/Chat/Chat.js
+++ src/views/Chat/Chat.js
@@ -132,16 +132,35 @@ class Chat extends Component {
                   <Card className="mb-0">
                     <CardHeader id="headingOne">
                       <Button block color="link" className="text-left m-0 p-0" onClick={() => this.toggleAccordion(0)} aria-expanded={this.state.accordion[0]} aria-controls="collapseOne">
-                        <h5 className="m-0 p-0">Collapsible Group Item #1</h5>
+                        <h5 className="m-0 p-0">I65</h5>
                       </Button>
                     </CardHeader>
                     <Collapse isOpen={this.state.accordion[0]} data-parent="#accordion" id="collapseOne" aria-labelledby="headingOne">
                       <CardBody>
-                        1. Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non
-                        cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird
-                        on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
-                        nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft
-                        beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        <table border={1}>
+                          <thead>
+                            <th>STT</th>
+                            <th>Tên người dùng</th>
+                            <th>Tr.thái đăng nhập</th>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>1</td>
+                              <td>1</td>
+                              <td>Đang đăng nhập</td>
+                            </tr>
+                            <tr>
+                              <td>1</td>
+                              <td>1</td>
+                              <td>Đang đăng nhập</td>
+                            </tr>
+                            <tr>
+                              <td>1</td>
+                              <td>1</td>
+                              <td>Đang đăng nhập</td>
+                            </tr>
+                          </tbody>
+                        </table>
                       </CardBody>
                     </Collapse>
                   </Card>
